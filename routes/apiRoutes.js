a8342b9059ecdaf229036c7fd8e64cff75fb083c
Mallika <mailtomallika2009@gmail.com>
2019-04-03 10:21:49 -0700

    initial UI files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,21 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+   
+  //Validate customers login and pwd. 
+  app.post("/api/login",/* passport.authenticate("local"),*/ function(req, res) {
+    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
+    // So we're sending the user back the route to the members page because the redirect will happen on the front end
+    // They won't get this or even be able to access this page if they aren't authed
+    res.json("/index");
   });
+
+  // Get all examples
+  // app.get("/api/examples", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.json(dbExamples);
+  //   });
+  // });
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
