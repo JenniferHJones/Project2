9e1f715f3cde3ebbcfdff5b178ed23a3059aebc8
JJ <jenjones@ucdavis.edu>
2019-04-07 09:56:29 -0700

    updated all files associated with passport.js authentication

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,7 @@
--- Data for customers table
-USE tradefy;
-INSERT INTO Customers (fullName,tradeAcct ,email, password)
-VALUES 
-    ('Mallika','TFY03564', 'mallika@email.com', 'mallika'),
-    ('Ria', 'TFY09235','ria@email.com', 'ria')
-;
+-- Drops the tradefy if it exists currently --
+DROP DATABASE IF EXISTS tradefy;
+-- Creates the "todolist" database --
+CREATE DATABASE tradefy;
 
-USE tradefy;
-INSERT INTO CustomerBankAccts (bankName, bankAcctNo, billingAddress,zip,CustomerId)
-VALUES 
-    ('JP Morgan Chase', '150487303', '1254 West El Camino Ave' , 95833,1),
-    ('Bank of America', '265345987', '2341 Truxel Road', 95834,2)
-;
+
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,9 @@
 var db = require("../models");
 var passport = require("../config/passport");
+var regAuth = require("../config/middleware/registerAuth");
 
 module.exports = function(app) {
-  app.post("/api/login",/* passport.authenticate("local"),*/ function(req, res) {
-    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
-    // So we're sending the user back the route to the members page because the redirect will happen on the front end
-    // They won't get this or even be able to access this page if they aren't authed
-    res.json("/index");
-  })
 
-  //============== CODE PROVIDED FOR US BY PROJECT ===================
   // Get bank account details
   app.get("/api/bankaccount/:id", function(req, res) {
    
@@ -47,8 +41,6 @@ app.get("/api/transfer", function(req, res) {
   }).then(function(dbTransfer) {
     res.json(dbTransfer);
   });
-
-
 });
 
 // save transfer amount in database
@@ -59,7 +51,6 @@ app.post("/api/transfer", function(req, res) {
   });
 });
 
-
 // save transfer amount in database
 app.post("/api/setupacct", function(req, res) {
   console.log(req.body, "req.body********");
@@ -68,7 +59,59 @@ app.post("/api/setupacct", function(req, res) {
   });
 });
 
+//======================= PASSPORT ==============================
+  // Using the passport.authenticate middleware with our local strategy. If customer has valid login
+  // credentials, send them to the market page, otherwise the customer will be sent an error
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
+    console.log("request body", req.body);
+    // Since we're doing a POST with javascript, we can't redirect that post into a GET request, so
+    // send customer back the route to the sign in page because the redirect will happen on the front end.
+    // They won't be able to access this page if they aren't authorized
+    res.json("/market");
+  });
 
+  // Route for registering a customer. The customer's password is automatically hashed and stored securely thanks to
+  // how we configured the Sequelize Customer Model. If the customer is created successfully, proceed to log the customer in,
+  // otherwise send back an error
+  app.post("/api/register", regAuth, function (req, res) {
+    console.log("this is the request.body", req.body);
+    db.Customer.create({
+      fName: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    })
+    .then(function () {
+      res.send("Success");
+    })
+      .catch(function (err) {
+        console.log(err);
+        res.json(err);
+        // res.status(422).json(err.errors[0].message);
+      });
+  });
+
+  // Route for logging customer out
+  app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+
+  // Route for getting some data about our customer to be used client side
+  app.get("/api/customer_data", function (req, res) {
+    if (!req.customer) {
+      // If the customer is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the customer's name, email, and id
+      res.json({
+        name: req.customer.name,
+        email: req.customer.email,
+        id: req.customer.id
+      });
+    }
+  });
+  
+  //============== CODE PROVIDED FOR US BY PROJECT ===================
   // Create a new example
   app.post("/api/examples", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
@@ -84,53 +127,4 @@ app.post("/api/setupacct", function(req, res) {
       res.json(dbExample);
     });
   });
-
-  //======================= PASSPORT ==============================
-  // Using the passport.authenticate middleware with our local strategy. If user has valid login
-  // credentials, send them to the members page, otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    // Since we're doing a POST with javascript, we can't redirect that post into a GET request, so
-    // send user back the route to the members page because the redirect will happen on the front end.
-    // They won't get this or even be able to access this page if they aren't authorized
-    res.json("/members");
-  });
-
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
-    console.log(req.body);
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
-    })
-      .then(function() {
-        res.redirect(307, "/api/login");
-      })
-      .catch(function(err) {
-        console.log(err);
-        res.json(err);
-        // res.status(422).json(err.errors[0].message);
-      });
-  });
-
-  // Route for logging user out
-  app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/");
-  });
-
-  // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      // Otherwise send back the user's email and id
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
-    }
-  });
-};
+}
